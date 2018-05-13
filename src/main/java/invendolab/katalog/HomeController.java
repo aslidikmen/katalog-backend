933c5593eb611a9ceec516be660e6d68bb4a8c9c
Ahmet Hasırcıoğlu <ahasircioglu@gmail.com>
2018-05-13 15:29:14 +0300

    add homeController for home page message

---
+++ src/main/java/invendolab/katalog/HomeController.java
@@ -0,0 +1,42 @@
+package invendolab.katalog;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
+@RestController
+@Api(value = "home", description = "Home screen to hello")
+public class HomeController {
+
+    //TODO -> when dev and api environments are set, link this with global variables in heroku
+
+    //@Value("${HOMEPAGE_MESSAGE:local}")
+    //String homepageMessage;
+
+    @ApiOperation(value = "Only saying hello to everyone.")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully") })
+    @GetMapping("/")
+    public String home(){
+
+        return "<body style=\"background-color:#F6F6F6\">" +
+                "<link href=\"https://fonts.googleapis.com/css?family=Nunito+Sans\" rel=\"stylesheet\">" +
+                "<h1 style=\"font-family: 'Nunito Sans', sans-serif; color:#3B3B3B\">" +
+                "<br /><br /><br />" +
+                "<center>" +
+                "Hello from Katalog API "; /* +
+                "through " +
+                homepageMessage +
+                " environment." +
+                "</center>" +
+                "</h1>" +
+                "</body>"; */
+
+    }
+
+}
